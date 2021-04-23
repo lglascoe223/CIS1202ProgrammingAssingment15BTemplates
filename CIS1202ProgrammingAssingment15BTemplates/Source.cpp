75ade302f1a829fb165a455f1d8fb2c336d27eea
Leslie Glascoe <leslie.glascoe@sinclair.edu>
2021-04-23 16:54:13 -0400

    created a template for  float and double function to return half the value while rounding up or down the value.

--- CIS1202ProgrammingAssingment15BTemplates/Source.cpp
+++ CIS1202ProgrammingAssingment15BTemplates/Source.cpp
@@ -10,3 +10,33 @@
 
 using namespace std;
 
+template <class T>
+
+T half(T number) {
+
+	return round(number / 2.0);
+}
+
+int half(int number) {
+
+	double result;
+	result = static_cast<double>(number / 2.0);
+	return round(result);
+}
+
+
+int main() {
+	
+	double a = 7.0;
+	float b = 5.0;
+	int c = 3;
+
+	cout << half(a) << endl;
+	cout << half(b) << endl;
+	cout << half(c) << endl;
+
+
+
+	system("pause");
+	return 0;
+}
